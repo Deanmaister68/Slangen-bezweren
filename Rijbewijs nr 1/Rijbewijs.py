07967026303f88a9fd7b662a9822fec38e850ec7
Deabmaister68 <514109@vistacollege.nl>
2022-11-17 09:30:52 +0100

    not much

--- Rijbewijs nr 1/Rijbewijs.py
+++ Rijbewijs nr 1/Rijbewijs.py
@@ -1,5 +1,5 @@
 print("Hoe oud ben je?")
 leeftijd = int(input())
-if leeftijd > 18 :
+if leeftijd >= 18 :
     print("Je mag autorijden.")
 else: print("Je mag geen autorijden.")
---
+++ verlanglijst Sinterklaas/Sinterklaas.py
@@ -0,0 +1,2 @@
+print("welkom bij de sinterklaas verlanglijst! Hier kunt u cadeau's invullen dat uw op uw verlanlijst wilt. Als u klaar bent typed u KLAAR!")
+
