968b31286104f20f2d405431a86b59a998fb4449
Deabmaister68 <514109@vistacollege.nl>
2022-11-25 09:09:51 +0100

    Update Rijbewijs2.py

--- Rijbewijs nr 2/Rijbewijs2.py
+++ Rijbewijs nr 2/Rijbewijs2.py
@@ -1,3 +1,12 @@
+import datetime
+now = datetime.datetime.now()
+now
+datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
+print(now)
+
+
 print("waneer bent u geboren?")
 datum = input("Zet de datum dag/maand/jaar ")
-if datum =
+if datum >= now:
+    print()
+else: print()
